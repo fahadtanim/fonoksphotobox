ef341bd0e75226ff9bc09f58c3e3d72a950f2530
Md. Fahad-Uz-Zaman Khan <fahad.khan@dsinnovators.com>
2019-05-31 18:12:45 +0600

    updated

--- src/app/photobox/photobox-category/photobox-category.component.ts
+++ src/app/photobox/photobox-category/photobox-category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {SliderService} from '../../../services/SliderService';
+import {PageEvent} from '@angular/material/paginator';
 @Component({
   selector: 'app-photobox-category',
   templateUrl: './photobox-category.component.html',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoboxCategoryComponent implements OnInit {
 
-  constructor() { }
+  public slider: SliderService;
+  // MatPaginator Inputs
+  length = 100;
+  pageSize = 7;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
 
+  // MatPaginator Output
+  pageEvent: PageEvent;
+
+
+  constructor() { }
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+  }
   ngOnInit() {
+    this.slider = new SliderService('slider', 'slider-container', 'dot', 'active', 3);
+    this.slider.animate();
+    var x = document.getElementsByClassName('mat-paginator-page-size');
+    console.log(x);
+    console.log(x.length);
+  }
+
+  ngOnDestroy() {
+    this.slider.stopAnimate();
   }
 
 }
--- src/app/photobox/photobox.component.ts
+++ src/app/photobox/photobox.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {SliderService} from '../../services/SliderService';
+import {PageEvent} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-photobox',
@@ -8,11 +9,25 @@ import {SliderService} from '../../services/SliderService';
 })
 export class PhotoboxComponent implements OnInit {
   public slider: SliderService;
-  constructor() { }
+  // MatPaginator Inputs
+  length = 100;
+  pageSize = 7;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
 
+  // MatPaginator Output
+  pageEvent: PageEvent;
+
+
+  constructor() { }
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+  }
   ngOnInit() {
     this.slider = new SliderService('slider', 'slider-container', 'dot', 'active', 3);
     this.slider.animate();
+    var x = document.getElementsByClassName('mat-paginator-page-size');
+    console.log(x);
+    console.log(x.length);
   }
 
   ngOnDestroy() {
